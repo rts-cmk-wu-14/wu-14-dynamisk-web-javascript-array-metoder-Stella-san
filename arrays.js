f9187b89048bfb6ba6aa1f277ca3609e49397307
Rikke Gleerup <rikkeglee@gmail.com>
2025-08-25 13:23:31 +0200

    opgave 3b løst

--- arrays.js
+++ arrays.js
@@ -26,6 +26,8 @@ document.addEventListener("DOMContentLoaded", function () {
     // Check om der i dogsArray er en hund som hedder "Baxter".
     // Udskriv "true" eller "false" i konsollen afhængig af svaret.
     // console.log("Opgave 3b", ...)
+    console.log("Opgave 3b:", dogsArray.includes("Baxter"));
+    
     
     //     Opgave 4a
     // Check om der i catsArray er en kat som hedder "Bagheera".
